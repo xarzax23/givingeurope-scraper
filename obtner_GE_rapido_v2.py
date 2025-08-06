a3ab66465d230b4a05184ce7cdec54a6e3887a84
xarzax23 <gonzaloperez1994@gmail.com>
2025-08-06 09:50:36 +0200

    nueva version

--- obtner_GE_rapido_v2.py
+++ obtner_GE_rapido_v2.py
@@ -1,12 +1,12 @@
 import requests, csv, re, sys
 from bs4 import BeautifulSoup
 
-HEADERS = {   
+HEADERS = {
    'Accept': 'application/json, text/plain, */*',
+   'Accept-Language': 'es-ES,es;q=0.9',
    'Origin': 'https://www.givingeurope.com',
    'Referer': 'https://www.givingeurope.com',
-   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-
+   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
 }
 
 def extract_pgid_from_html(html):
